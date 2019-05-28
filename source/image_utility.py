3ef1257f7157107863c72399264bbe73f87bf9dd
Luís Eduardo Rozante <luis.eduardo.rozante@gmail.com>
2019-05-28 15:20:58 -0300

    Height map generation and README
    - Added a levels function, that is currently being used with hard coded values to generate normal maps.
    
    - Updated README.
    
    - Added some logs about progress on main.

---
+++ source/image_utility.py
@@ -0,0 +1,46 @@
+# Contains functions used by multiple images.
+import matplotlib as mpl
+import numpy as np
+import imageio
+
+def as_monochrome(rgb_image):
+
+    hsv_image = mpl.colors.rgb_to_hsv(rgb_image)
+
+    return hsv_image[:,:, 2]
+
+def gamma_correction (midtone):
+
+    gamma = 1
+
+    midtone_normalized = midtone / 255
+
+    if midtone < 128:
+        midtone_normalized = midtone_normalized * 2
+        gamma = 1 + ( 9 * ( 1 - midtone_normalized ) )
+        gamma = min( gamma, 9.99 )
+    elif midtone > 128:
+        midtone_normalized = ( midtone_normalized * 2 ) - 1
+        gamma = 1 - midtone_normalized
+        gamma = max( gamma, 0.01 )
+
+    return (1 / gamma)
+
+def level_image(gray_scale, in_shadown, in_high, midtones, out_shadown, out_high):
+
+     # Creates the new image.
+    leveled_image = gray_scale
+
+    # Calculates the gamma correction to be used.
+    gamma = gamma_correction(midtones)
+
+    # Levels the input values.
+    leveled_image = 255 * np.clip((leveled_image - in_shadown) / (float)(in_high - in_shadown), 0, 255)
+
+    # Iterates the grayscale image assigning the leveled values to the new image.
+    leveled_image = 255 * np.power((leveled_image / 255.0), gamma)
+
+    # Levels the output values.
+    leveled_image = ((leveled_image / 255.0) * (float)(out_high - out_shadown )) + out_shadown
+                
+    return leveled_image
--- source/main.py
+++ source/main.py
@@ -5,13 +5,9 @@ import numpy as np
 import imageio
 import pathlib
 import sys
+import time
 
-
-def as_monochrome(rgb_image):
-    hsv_image = mpl.colors.rgb_to_hsv(rgb_image)
-
-    return hsv_image[:, :, 2]
-
+import image_utility as util
 
 def main():
     if len(sys.argv) != 2:
@@ -26,9 +22,63 @@ def main():
 
     image = imageio.imread(image_path)
 
-    out_image = as_monochrome(image)
+    print("# (TODO) Tiling image...")
 
-    imageio.imwrite("out.png", out_image)
+    #           #
+    #   TODO    #
+    #           #
+
+    print("# Generating grayscale...")
+
+    # Stores the start time of the operation.
+    start_t = time.time()
+
+    # Generates a grayscale image to be used to create height and roughness maps.
+    grayscale = util.as_monochrome(image)
+
+    # Measures the amount of time spent.
+    end_t = time.time()
+    print("DONE! (Time spent: {:.2f})".format(end_t - start_t));
+
+    print("# Generating height map...")
+    print("## Aplying levels...")
+
+    # Stores the start time of the operation.
+    start_t = time.time()
+
+    # Generates the height map.
+    height_map = util.level_image(grayscale, 90, 255, 70, 0, 255)
+
+    # Measures the maount of time spent.
+    end_t = time.time()
+    print("DONE! (Time spent: {:.2f})".format(end_t - start_t));
+
+    print("# Saving files...")
+
+    print("# (TODO) Generating normal map...")
+
+    #           #
+    #   TODO    #
+    #           #
+
+    print("# (TODO) Generating roughness map...")
+
+    #           #
+    #   TODO    #
+    #           #
+
+    # Stores the start time of the operation.
+    start_t = time.time()
+
+    # (TODO) Save tiling texture
+    imageio.imwrite("grayscale.png", grayscale.astype(np.uint8))
+    imageio.imwrite("height.png", height_map.astype(np.uint8))
+    # (TODO) Save normal map
+    # (TODO) Save roughness map
+
+    # Measures the maount of time spent.
+    end_t = time.time()
+    print("DONE! (Time spent: {:.2f})".format(end_t - start_t));
 
 
 main()
